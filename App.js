c110a2ae8d198a18d653b5595f03e7c9c19eddf6
AndreSantos0423 <123743123+AndreSantos0423@users.noreply.github.com>
2023-03-11 12:25:49 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import { useState } from "react";
 
-function MyButton({label}) {
+function MyButton({label , onClick}) {
   return (
-    <button className="CalcButton">
+    <button className="Calculator" onClick={onClick}>
       {label}
     </button>
   );
@@ -16,37 +17,67 @@ function Display({display}) {
   );
 }
 
-  export default function App() {
-    const numberClickHandler = (e) => {
+function App() {
+
+  const[disp, setDisp] = useState();
+
+
+  const numberClickHandler = (e) => {
       e.preventDefault();
       const value = e.target.innerHTML;
 
       console.log(value);
+      setDisp(value);
+
+    }
+
+    const operatorClickHandler = (e) => {
+      e.preventDefault();
+      const value = e.target.innerHTML;
+
+      console.log(value);
+      setDisp(value);
+    }
+
+    const equalClickHandler = (e) => {
+      e.preventDefault();
+      const value = e.target.innerHTML;
+
+      console.log(value);
+      setDisp(value);
+    }
+
+    const clearClickHandler = (e) => {
+      e.preventDefault();
+      const value = e.target.innerHTML;
+
+      console.log(value);
+      setDisp(value);
     }
     return(
     <div className="App">
-      <h1> Welcome to my app </h1>
-      <Display  display={0}/>
+      <h1> My Calculator </h1>
+      <Display display={0}/>
       <div className="ButtonContainer">
-      <MyButton label={7} onClick={numberCLickHandler}/>
-      <MyButton label={8} onClick={numberCLickHandler}/>
-      <MyButton label={9} onClick={numberCLickHandler}/>
-      <MyButton label={"+"} onClick={operatorCLickHandler}/>
-      <MyButton label={4} onClick={numberCLickHandler}/>
-      <MyButton label={5} onClick={numberCLickHandler}/>
-      <MyButton label={6} onClick={numberCLickHandler}/>
-      <MyButton label={"-"} onClick={operatorCLickHandler}/>
-      <MyButton label={1} onClick={numberCLickHandler}/>
-      <MyButton label={2} onClick={numberCLickHandler}/>
-      <MyButton label={3} onClick={numberCLickHandler}/>
-      <MyButton label={"*"} onClick={operatorCLickHandler}/>
-      <MyButton label={"C"} onClick={operatorCLickHandler}/>
-      <MyButton label={0} onClick={numberCLickHandler}/>
-      <MyButton label={"="} onClick={operatorCLickHandler}/>
-      <MyButton label={"+"} onClick={operatorCLickHandler}/>
+      <MyButton label={7} onClick={numberClickHandler}/>
+      <MyButton label={8} onClick={numberClickHandler}/>
+      <MyButton label={9} onClick={numberClickHandler}/>
+      <MyButton label={"+"} onClick={operatorClickHandler}/>
+      <MyButton label={4} onClick={numberClickHandler}/>
+      <MyButton label={5} onClick={numberClickHandler}/>
+      <MyButton label={6} onClick={numberClickHandler}/>
+      <MyButton label={"-"} onClick={operatorClickHandler}/>
+      <MyButton label={1} onClick={numberClickHandler}/>
+      <MyButton label={2} onClick={numberClickHandler}/>
+      <MyButton label={3} onClick={numberClickHandler}/>
+      <MyButton label={"*"} onClick={operatorClickHandler}/>
+      <MyButton label={"C"} onClick={clearClickHandler}/>
+      <MyButton label={0} onClick={numberClickHandler}/>
+      <MyButton label={"="} onClick={equalClickHandler}/>
+      <MyButton label={"+"} onClick={operatorClickHandler}/>
       </div>
     </div>
   );
 }
 
-
+export default App;
